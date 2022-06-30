8713f746612c102c6ade1be692332c3e593af743
Maksim Galimov <maximgalimov96@gmail.com>
2022-07-01 02:03:43 +0300

    fix cors

--- src/SimonsVoss.LSM.Web/Program.cs
+++ src/SimonsVoss.LSM.Web/Program.cs
@@ -28,8 +28,8 @@ if (app.Environment.IsDevelopment())
     app.UseSwaggerUI();
 }
 
-app.UseHttpsRedirection();
 app.UseCors();
+app.UseHttpsRedirection();
 app.UseAuthorization();
 
 app.MapControllers();
